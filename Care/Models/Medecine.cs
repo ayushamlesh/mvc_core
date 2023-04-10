800b5b9e1e43b5f1efff4a11f72a3c8c713f9cd7
ayushamleh <ayushamlesh@gmail.com>
2023-04-10 12:03:38 +0530

    initial push

---
+++ Care/Models/Medecine.cs
@@ -0,0 +1,15 @@
+﻿namespace Care.Models
+{
+    public class Medecine
+    {
+        //here we will write the property of our table columns here
+
+        //for keeping required constrains we will user data antonations
+
+        public string BatchId { get; set; }
+        public string MedName { get; set; }
+        public string Quantity { get; set;}
+        public string Price { get; set; }
+        public DateOnly Expire { get; set;}
+    }
+}
